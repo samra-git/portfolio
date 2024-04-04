def1880cafcadc017bf0a13e94032fffc8e0e3e0
Samra Harbi <samra.harbi78@gmail.com>
2024-04-04 11:51:19 +0200

    routage carrousel et page projet opé

--- src/components/ProjetSlide.jsx
+++ src/components/ProjetSlide.jsx
@@ -9,29 +9,21 @@ import ninaCarducci from '../assets/ninaCarducci.png'
 import events from '../assets/724events.png'
 import Carousel from "./Carousel"
 import { Link } from "react-router-dom"
+import data from '../data/data.json'
 
 function ProjetSlide() {
   return (
-    <div className="projets">
+    < div className="projets">
       <h1 id="projets">Projets</h1>
 
       <Carousel>
-        <div className="carouselBox">
-          <Link to="/projet1" className="carousel-cover"><img src={booki} />
-          <div className="carousel-content">
-            <h2 className="carousel-title">Booki</h2>
-            <p >Intégrer l'interface  responsive d'une agence de voyage avec HTML et CSS</p>
-          </div>
-          </Link>
-          
-
-        </div>
-        <Link to="/projet2" className="carouselBox"><img src={ohmyfood} /></Link>
-        <Link to="/projet3" className="carouselBox"><img src={sophieBluel} /></Link>
-        <Link to="/projet4" className="carouselBox"><img src={kasa} /></Link>
-        <Link to="/projet5" className="carouselBox"><img src={ninaCarducci} /></Link>
-        <Link to="/projet6" className="carouselBox"><img src={events} /></Link>
-        <Link to="/projet7" className="carouselBox"><img src={argentBank} /></Link>
+        <Link to="/projets/projet/1" className="carouselBox"><img src={booki} /></Link>
+        <Link to="/projets/projet/2" className="carouselBox"><img src={ohmyfood} /></Link>
+        <Link to="/projets/projet/3" className="carouselBox"><img src={sophieBluel} /></Link>
+        <Link to="/projets/projet/4" className="carouselBox"><img src={kasa} /></Link>
+        <Link to="/projets/projet/5" className="carouselBox"><img src={ninaCarducci} /></Link>
+        <Link to="/projets/projet/6" className="carouselBox"><img src={events} /></Link>
+        <Link to="/projets/projet/7" className="carouselBox"><img src={argentBank} /></Link>
       </Carousel>
     </div>
   );
